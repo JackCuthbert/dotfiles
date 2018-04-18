ac4d39c7a5cdbb850fd1ecd966976db320692d63
Jack Cuthbert <jckcthbrt@gmail.com>
2018-04-18 15:13:23 +1000

    Remove duplicate alias, add rspec screenshot workaround

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -5,15 +5,17 @@
 #######
 alias zshsource="source ~/.zshrc"
 alias clterm="clear && zshsource"
-alias v="nvim"
 alias cl="clear"
 alias g="git"
 alias dotfiles="cd ~/.dotfiles"
 alias record="asciinema rec"
 alias l="exa -ghl"
 alias la="exa -ghla"
-alias v=nvim
+alias v="nvim"
 
 # Arch Specific
 alias upgrade-aur="sudo aura -Akua"
 alias upgrade-pacman="sudo pacman -Syu"
+
+# Disable rspec in-terminal screenshots
+alias rspec="CAPYBARA_INLINE_SCREENSHOT=artifact bundle exec rspec"
