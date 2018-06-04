2ebb17adb5282d5f5f575766234f34259d03d93e
Jack Cuthbert <jckcthbrt@gmail.com>
2018-06-05 09:11:30 +1000

    Add warp drive

--- zsh/zplug.mac.sh
+++ zsh/zplug.mac.sh
@@ -1,9 +1,15 @@
+#######
+#
+# ZPLUG
+#
+#######
 source ~/.zplug/init.zsh
 
 zplug "b4b4r07/emoji-cli"
 zplug "mafredri/zsh-async", from:github
 zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
 zplug "plugins/thefuck", from:oh-my-zsh
+zplug 'mfaerevaag/wd', as:command, use:"wd.sh", hook-load:"wd() { . $ZPLUG_REPOS/mfaerevaag/wd/wd.sh }"
 zplug "zsh-users/zsh-history-substring-search", defer:3
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-completions"
