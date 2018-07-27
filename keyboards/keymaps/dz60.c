285674bafc6fc1612e85855d5327382048a23a42
Jack Cuthbert <jckcthbrt@gmail.com>
2018-07-27 10:31:38 +1000

    Use i3(ish) bindings for chunkwm on dz60

--- keyboards/keymaps/dz60.c
+++ keyboards/keymaps/dz60.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | Shift           |
    * |-----------------------------------------------------------------------------------------+
-   * |  FN1  |  Alt  |  Cmd  |              Space              |  Cmd  |  Alt  |  FN2  |  FN3  |
+   * |  FN1  |  Cmd  |  Alt  |              Space              |  Alt  |  Cmd  |  FN2  |  FN3  |
    * `-----------------------------------------------------------------------------------------'
    */
   LAYOUT(
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,    KC_I,  KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,    KC_J,    KC_K,  KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, NO_OP,   KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, NO_OP,
-    MO(1),   KC_LALT, KC_LGUI, SPACE, SPACE, SPACE, KC_RGUI, KC_RALT, NO_OP, MO(2),   MO(3)
+    MO(1),   KC_LGUI, KC_LALT, SPACE, SPACE, SPACE, KC_RALT, KC_RGUI, NO_OP, MO(2),   MO(3)
   ),
 
   /* [1] Main function layer
