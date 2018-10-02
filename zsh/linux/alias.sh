8cbab22c4508bee6884640fef24d5bc151aac5bd
Jack Cuthbert <jckcthbrt@gmail.com>
2018-10-02 20:46:28 +1000

    Update linux aliases

--- zsh/linux/alias.sh
+++ zsh/linux/alias.sh
@@ -1,4 +1,10 @@
-alias upgrade-aur="sudo aura -Akua"
-alias upgrade-pacman="sudo pacman -Syu"
+# Assumes aura-bin is installed
+
+# Package management
+alias upgrade-aur="sudo aura -Aau"
+alias upgrade-pacman="sudo aura -Syuu"
+alias packages-cleanup="sudo pacman -Rns $(pacman -Qtdq)"
 alias packages-pacman="comm -23 <(pacman -Qeq | sort) <(pacman -Qgq base base-devel | sort) | less"
+
+# becuase who wants to type that
 alias music="ncmpcpp"
