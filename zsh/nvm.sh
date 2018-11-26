81783b12b6abf24e7d4d036c39227248467687a1
Jack Cuthbert <jckcthbrt@gmail.com>
2018-11-26 21:29:55 +1100

    Lazy load nvm

--- zsh/nvm.sh
+++ zsh/nvm.sh
@@ -3,6 +3,20 @@
 # NVM
 #
 #######
+# borrowed from https://gist.github.com/fl0w/07ce79bd44788f647deab307c94d6922#gistcomment-2650045
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
+# Add every binary that requires nvm, npm or node to run to an array of node globals
+NODE_GLOBALS=(`find $NVM_DIR/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+NODE_GLOBALS+=("node")
+NODE_GLOBALS+=("nvm")
+
+# Lazy-loading nvm + npm on node globals call
+load_nvm () {
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+}
+
+# Making node global trigger the lazy loading
+for cmd in "${NODE_GLOBALS[@]}"; do
+  eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
+done
