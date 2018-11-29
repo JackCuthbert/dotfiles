24d6f70b11e049986a9a76d29c68e6d2d64fa83f
Jack Cuthbert <jckcthbrt@gmail.com>
2018-11-29 20:08:04 +1100

    Update linux aliases

--- zsh/linux/alias.sh
+++ zsh/linux/alias.sh
@@ -3,8 +3,15 @@
 # Package management
 alias upgrade-aur="sudo aura -Aau"
 alias upgrade-pacman="sudo aura -Syuu"
-alias packages-cleanup="sudo pacman -Rns $(pacman -Qtdq)"
-alias packages-pacman="comm -23 <(pacman -Qeq | sort) <(pacman -Qgq base base-devel | sort) | less"
+
+# remove orphaned packages
+alias pacman-cleanup="sudo pacman -Rns $(pacman -Qtdq)"
+
+# list installed packages _not_ in the base or base-devel groups
+alias pacman-list="comm -23 <(pacman -Qeq | sort) <(pacman -Qgq base base-devel | sort) | less"
 
 # becuase who wants to type that
 alias music="ncmpcpp"
+
+# open an image in feh in a sane way
+alias feh-view="feh --scale-down --image-bg black --sort name --no-menus --conversion-timeout 1 --force-aliasing --title '[%u/%l] %n (%wx%h)'"
