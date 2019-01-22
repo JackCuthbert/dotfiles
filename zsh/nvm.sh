6cd367f939490cc71f4e68ae5de56d8efde164b1
Jack Cuthbert <jckcthbrt@gmail.com>
2019-01-23 10:29:11 +1100

    Disable nvm lazy load, enable .nvmrc file support

--- zsh/nvm.sh
+++ zsh/nvm.sh
@@ -3,20 +3,27 @@
 # NVM
 #
 #######
-# borrowed from https://gist.github.com/fl0w/07ce79bd44788f647deab307c94d6922#gistcomment-2650045
 export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use
 
-# Add every binary that requires nvm, npm or node to run to an array of node globals
-NODE_GLOBALS=(`find $NVM_DIR/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
-NODE_GLOBALS+=("node")
-NODE_GLOBALS+=("nvm")
+# place this after nvm initialization!
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
 
-# Lazy-loading nvm + npm on node globals call
-load_nvm () {
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
 }
-
-# Making node global trigger the lazy loading
-for cmd in "${NODE_GLOBALS[@]}"; do
-  eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
-done
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
