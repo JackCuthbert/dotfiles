dcbdb62c463daad9b419c5252b5bcaad83afec7b
Jack Cuthbert <jckcthbrt@gmail.com>
2019-04-01 09:51:02 +1100

    Improve formatting of zplug config

--- zsh/zplug.sh
+++ zsh/zplug.sh
@@ -1,16 +1,39 @@
 # Source installed zplug
 source ~/.zplug/init.zsh
 
-# Plugins
-zplug "mafredri/zsh-async", from:github
-zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
-zplug "plugins/thefuck", from:oh-my-zsh
-zplug "mfaerevaag/wd", as:command, use:"wd.sh", hook-load:"wd() { . $ZPLUG_REPOS/mfaerevaag/wd/wd.sh }"
-zplug "zsh-users/zsh-history-substring-search", defer:3
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+# Support async operations
+zplug "mafredri/zsh-async", \
+  from:github,\
+  use:"async.zsh"
+
+# Prompt theme
+zplug "sindresorhus/pure", \
+  use:pure.zsh, \
+  from:github, \
+  as:theme
+
+# Fix last command with `fuck`
+zplug "plugins/thefuck", \
+  from:oh-my-zsh
+
+# NOTE: Currently borked
+# zplug "mfaerevaag/wd", \
+#   as:command, \
+#   use:"wd.sh", \
+#   hook-load:"wd() { . $ZPLUG_REPOS/mfaerevaag/wd/wd.sh }"
+
+zplug "zsh-users/zsh-history-substring-search", \
+  defer:3
+
+zplug "zsh-users/zsh-syntax-highlighting", \
+  defer:2
+
 zplug "zsh-users/zsh-completions"
+
 zplug "zsh-users/zsh-autosuggestions"
+
 zplug "djui/alias-tips"
+
 zstyle ":completion:*" matcher-list "m:{a-zA-Z}={A-Za-z}"
 
 # Load plugins
