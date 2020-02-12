2a3d7e3272050ef95f6c04d9758c40ed9d1a4d22
Jack Cuthbert <jckcthbrt@gmail.com>
2020-02-13 10:27:04 +1100

    fix: remove conflicting keybinds

--- config/zsh/vi-mode.zsh
+++ config/zsh/vi-mode.zsh
@@ -4,9 +4,6 @@ bindkey -v
 # Remove the 0.4s delay switching vi modes
 export KEYTIMEOUT=1 # reduces to 0.1
 
-bindkey '^P' up-history
-bindkey '^N' down-history
-
 # Use the delete key properly
 bindkey '^[[3~' delete-char
 bindkey '^[3;5~' delete-char
