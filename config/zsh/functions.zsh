2e225ff632f43a0a70f49052b8fb1fa2edb55d6a
Jack Cuthbert <jckcthbrt@gmail.com>
2020-04-01 09:46:58 +1100

    feat: add zoxide to replace ctrl-p fzf

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -1,15 +1,3 @@
-# https://danishpraka.sh/2018/08/03/command-line-dir-switcher.html
-# Requires: fzf
-function quick_find () {
-    dir=$(find ~/Code -type d -not -path '*/\.*' -maxdepth 1 | fzf)
-    cd $dir
-    starship_precmd
-    zle reset-prompt
-}
-
-zle -N quick_find_widget quick_find
-bindkey "^P" quick_find_widget
-
 # Open something and disown the process
 function open () {
     xdg-open "$*" &
