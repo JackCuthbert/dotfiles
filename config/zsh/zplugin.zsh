0d674f95cd65a1c32598360d2cf64c51325fff3b
Jack Cuthbert <jckcthbrt@gmail.com>
2020-07-06 14:12:18 +1000

    refactor: update zplugin to zinit
    Migrate with:
    
    mkdir ~/.zinit
    git clone https://github.com/zdharma/zinit.git ~/.zinit/bin

--- config/zsh/zplugin.zsh
+++ config/zsh/zplugin.zsh
@@ -1,27 +1,25 @@
-source ~/.zplugin/bin/zplugin.zsh
-autoload -Uz _zplugin
-(( ${+_comps} )) && _comps[zplugin]=_zplugin
+source ~/.zinit/bin/zinit.zsh
 
 # Highlighting
-zplugin ice silent wait"0" atinit"zpcompinit; zpcdreplay"
-zplugin light zdharma/fast-syntax-highlighting
+zinit ice silent wait"0" atinit"zpcompinit; zpcdreplay"
+zinit light zdharma/fast-syntax-highlighting
 
 # Suggestions/completions
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-zplugin ice silent wait"0" blockf
-zplugin light zsh-users/zsh-completions
-zplugin ice silent wait"0" atload"_zsh_autosuggest_start"
-zplugin light zsh-users/zsh-autosuggestions
+zinit ice silent wait"0" blockf
+zinit light zsh-users/zsh-completions
+zinit ice silent wait"0" atload"_zsh_autosuggest_start"
+zinit light zsh-users/zsh-autosuggestions
 
 # nvm and npm completions
-zplugin ice silent wait"0"
-zplugin light lukechilds/zsh-nvm
-zplugin ice silent wait"0"
-zplugin light lukechilds/zsh-better-npm-completion
+zinit ice silent wait"0"
+zinit light lukechilds/zsh-nvm
+zinit ice silent wait"0"
+zinit light lukechilds/zsh-better-npm-completion
 
 # Git
-zplugin snippet OMZ::plugins/git/git.plugin.zsh
+zinit snippet OMZ::plugins/git/git.plugin.zsh
 
 # thefuck
-zplugin ice wait"1" lucid
-zplugin light laggardkernel/zsh-thefuck
+zinit ice wait"1" lucid
+zinit light laggardkernel/zsh-thefuck
