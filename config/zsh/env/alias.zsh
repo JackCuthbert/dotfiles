b0c1e050d168e3675dea8ede2e19716804249b7d
Jack Cuthbert <jckcthbrt@gmail.com>
2020-07-14 17:52:48 +1000

    refactor: remove unused variables, cleanup files, update alacritty title

--- config/zsh/env/alias.zsh
+++ config/zsh/env/alias.zsh
@@ -1,33 +1,20 @@
-#######
-#
-# Alias
-#
-#######
+alias c='docker-compose'
 alias cl="clear"
+alias d='docker'
+alias da='direnv allow'
+alias f="zi"
 alias g="git"
+alias l="exa --group-directories-first -gl --git"
+alias la="l -a"
 alias v="nvim"
 
-# Common directories
-alias f="zi"
-
-# Docker
-alias d='docker'
-alias c='docker-compose'
+# vim-like
+alias :q='exit'
+alias :wq='exit'
+alias :x='exit'
 
 # remove orphaned packages
 alias pacman-cleanup="echo \"Consider using aura -Oj\"; sleep 3; sudo pacman -Rns $(pacman -Qtdq)"
 
 # list installed packages _not_ in the base or base-devel groups
 alias pacman-list="comm -23 <(pacman -Qeq | sort) <(pacman -Qgq base base-devel | sort) | less"
-
-# file listing
-alias l="exa --group-directories-first -gl --git"
-alias la="l -a"
-
-# vim-like commands
-alias :q='exit'
-alias :wq='exit'
-alias :x='exit'
-
-# direnv
-alias da='direnv allow'
--- config/zsh/env/env.zsh
+++ config/zsh/env/env.zsh
@@ -1,15 +1,9 @@
 # Generic env vars
-export QT_QPA_PLATFORMTHEME=qt5ct
 export EDITOR="nvim"
 export TERMINAL="alacritty"
 
 # Use pixel-perfect scrolling in firefox
 export MOZ_USE_XINPUT2=1
-
-# Fix VSCode not being able to send files to trash
-# https://github.com/Microsoft/vscode/issues/13189
-export ELECTRON_TRASH=gio
-
 
 # thefuck plugin from zplug
 export THEFUCK_REQUIRE_CONFIRMATION="false"
@@ -20,11 +14,6 @@ export ZSH_AUTOSUGGEST_USE_ASYNC="true"
 
 # Disable scaling for alacritty
 export WINIT_HIDPI_FACTOR=1
-
-# fff configuration
-export FFF_COL1=2 # green directories
-export FFF_COL2=5 # pink statusbar
-export FFF_COL3=3 # yellow selection
 
 # Make NVM fast again
 export NVM_LAZY_LOAD=false
