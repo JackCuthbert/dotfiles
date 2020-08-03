89a94fae7ce578f66996760d370c1c514714c6f4
Jack Cuthbert <jckcthbrt@gmail.com>
2020-08-03 18:22:06 +1000

    fix: screen blank command wrong

--- config/xorg/xinit.sh
+++ config/xorg/xinit.sh
@@ -9,7 +9,7 @@ systemctl --user import-environment DISPLAY
 
 # disable blanking and enable dpms features, 5 min timeouts
 xset s off
-xset s no blank
+xset s noblank
 xset dpms 300 300 300
 
 # set capslock to be control
