b360c19c66f398ce000ad1cc32b73b7af2613d4a
Jack Cuthbert <jckcthbrt@gmail.com>
2024-05-23 15:17:32 +1000

    fix: increase delay before key-repeat

--- stow/scripts/defaults.sh
+++ stow/scripts/defaults.sh
@@ -5,5 +5,5 @@ defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.intellij.ce ApplePressAndHoldEnabled -bool false
 
 # fast key repeat
-defaults write -g InitialKeyRepeat -int 10
+defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 1
